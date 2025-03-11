12172d09586b5cc69a8a3b75c95c3ef0bb10476b
RaphaelB2024 <rbranston2024@elam.co.uk>
2025-03-11 11:49:08 +0000

    random area error

--- Assets/Scripts/PaperSpawner.cs
+++ Assets/Scripts/PaperSpawner.cs
@@ -7,6 +7,16 @@ public class PaperSpawner : MonoBehaviour
     //Create public array of objects to spawn
     public GameObject[] objectsToSpawn;
 
+    public GameObject minX;
+    public GameObject minY;
+    public GameObject maxX;
+    public GameObject maxY;
+
+    float randomx = 0;
+    float randomy = 0;
+
+    Vector3 randompos;
+
     float timeTillSpawn;            //Length to wait before spawning new object
     float timeSinceSpawn = 0.0f;    //Time since last object was spawned
 
@@ -36,11 +46,14 @@ public class PaperSpawner : MonoBehaviour
             //Instantiate spawns a gameObject - this case from objectsToSpawn
             //second parameter is where to spawn
             //third parameter is rotation, Quarternion.identity means no rotation
-            Instantiate(objectsToSpawn[selection], transform.position, Quaternion.identity);
+            Instantiate(objectsToSpawn[selection], randompos, Quaternion.identity);
 
             //After spawn, timer is reset and a new time until spawn is selected
             timeTillSpawn = Random.Range(minSpawnTime, maxSpawnTime);
             timeSinceSpawn = 0.0f;
+            randomx = Random.Range(minX.x, minX.x);
+            randomy = Random.Range(minY.y, maxY.y);
+            randompos = (randomx, randomy, 0);
         }
     }
 }
