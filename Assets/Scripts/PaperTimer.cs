add0e576adce9eb8b5ffba7c9fd5bb9f81a194b0
RaphaelB2024 <rbranston2024@elam.co.uk>
2025-03-13 14:38:30 +0000

    cooked

--- Assets/Scripts/PaperTimer.cs
+++ Assets/Scripts/PaperTimer.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PaperTimer : MonoBehaviour
 {
+    public LayerMask incinerator;
+    public GameObject hit;
     public float deleteTimer = 3;
     float Timer = 0;
     bool timerOn = true;
 
     private void Update()
     {
+        var ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        {
+            Debug.Log(hit.transform.gameObject);
+        }
+
         if (timerOn)
         {
             Timer += Time.deltaTime;
@@ -20,19 +29,6 @@ public class PaperTimer : MonoBehaviour
             }
         }
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Incinerator"))
-        {
-            Debug.Log("Entered Incinerator");
-            timerOn = false;
-        }
-    }
 
-   /* private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("Exit Incinerator");
-        timerOn = true;
-    }*/
 
 }
