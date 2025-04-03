bdd09cdc0df154e2da642b60a005d7be04a4d810
RaphaelB2024 <rbranston2024@elam.co.uk>
2025-04-03 10:39:19 +0100

    moving paper

--- Assets/Scripts/deleteClick.cs
+++ Assets/Scripts/deleteClick.cs
@@ -2,10 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class deleteClick : MonoBehaviour
+public class guideMover : MonoBehaviour
 {
+    public bool move = false;
+    public float speed = 3f;
+    public float rotateSpeed = 5f;
+    public Transform moveTarget;
+    public Transform rotateTarget;
+
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        move = true;
+    }
+
+    private void Update()
+    {
+        if (move)
+        {
+            Relocate();
+        }
+    }
+    void Relocate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveTarget.position, speed * Time.deltaTime);
+
+        var rotation = Quaternion.LookRotation(rotateTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
     }
 }
